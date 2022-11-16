b2dbfb40b88eab5fe2cd93ab62a90c5c46fc47e5
neelsnarayan <114615461+neelsnarayan@users.noreply.github.com>
2022-11-16 00:42:08 -0800

    Add files via upload
    newest uploads

--- clustering.py
+++ clustering.py
@@ -1,26 +1,27 @@
 import tensorflow.keras as keras
 from pyannote.core import Annotation, Segment
-from pyannote.metrics.diarization import DiarizationErrorRate
+from pyannote.metrics.diarization import DiarizationErrorRate, DiarizationPurity
 from sklearn.cluster import SpectralClustering
 import numpy as np
 import pandas as pd
+import time
 
 MODEL_NAME = "softmax_sparse_categorical_crossentropy"
 TEST_PATH_LABELS = "wav2spk_test.txt"
 TEST_PATH_DIR_OUT = "test_features/"
 
+
 def load_test_data():
-    """Loads training dataset from json file.
-        :param data_path (str): Path to json file containing data
-        :return X (ndarray): Inputs
-        :return y (ndarray): Targets
-    """
-    df = pd.read_csv(TEST_PATH_LABELS, sep=' ', header=None, names=["Audio File", "Speaker ID"])
-    df["Audio Features"] = df["Audio File"].apply(lambda filename: filename.split("/")[-1].replace(".wav", ".npy"))
+    df = pd.read_csv(
+        TEST_PATH_LABELS, sep=" ", header=None, names=["Audio File", "Speaker ID"]
+    )
+    df["Audio Features"] = df["Audio File"].apply(
+        lambda filename: filename.split("/")[-1].replace(".wav", ".npy")
+    )
 
     inputs = []
     labels = []
-    file_to_speaker_id = dict(zip(df['Audio Features'], df['Speaker ID']))
+    file_to_speaker_id = dict(zip(df["Audio Features"], df["Speaker ID"]))
     for j, filename in enumerate(file_to_speaker_id):
         try:
             input = np.load(TEST_PATH_DIR_OUT + filename)
@@ -40,32 +41,78 @@ def load_test_data():
 
 if __name__ == "__main__":
 
-    #load the model
+    # model = keras.Sequential()
+
+    # model.add(keras.layers.LSTM(64, return_sequences=True))
+    # model.add(keras.layers.LSTM(64))
+
+    # model.add(keras.layers.Dense(350, activation="relu"))
+    # model.add(keras.layers.Dropout(0.3))
+
+    # model.add(keras.layers.Dense(500))
+    # model.add(keras.layers.Activation(keras.activations.softmax))
+
+    # load the model
     model = keras.models.load_model(MODEL_NAME)
+    # print(model.layers[-1].output)
+    # print(len(model.layers))
+    print("Finished loading model")
 
-    #pop off the last dense + softmax layer (maybe change it to just pop off the softmax if possible) and recompile
-    model.pop()
+    # pop off the last dense + softmax layer (maybe change it to just pop off the softmax if possible) and recompile
+    # model.pop()
+    # print(model.layers[-1].output)
+    # print(len(model.layers))
+    # print("Popped off last dense + softmax")
     optimiser = keras.optimizers.Adam(learning_rate=0.0001)
-    model.compile(optimizer=optimiser,
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer=optimiser,
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
 
-    #generate embeddings for all test inputs
+    # load the test inputs
     test_inputs, test_labels = load_test_data()
+    print("Loaded test inputs")
 
-    #generate embeddings for all test inputs and cluster
+    # generate embeddings for all test inputs and cluster
     embeddings = model.predict(test_inputs, batch_size=32)
-    clustering = SpectralClustering(n_clusters=4, assign_labels='discretize', random_state=0).fit(embeddings)
+    print(test_inputs.shape)
+    print(embeddings.shape)
+    # labels = np.argmax(embeddings, axis=1)
+    print("Finished predictions")
+
+    start = time.time()
+    clustering = SpectralClustering(
+        n_clusters=4, assign_labels="discretize", random_state=0, verbose=True
+    ).fit(embeddings)
+    end = time.time()
+    print("Clustering time: {} seconds".format(end - start))
+    print("Finished clustering")
 
     gt = Annotation()
     predictions = Annotation()
 
-    for i in range(len(test_inputs)):
-        embedding = embeddings[i]
-        gt[Segment(i, i+1)] = test_labels[i]
-        predictions[Segment(i, i+1)] = clustering.labels_[i]
+    segment_sizes = [3, 7, 20, 50, 100, 200, 250, 350, 450, 500]
 
-    diarizationErrorRate = DiarizationErrorRate()
-    print("DER = {0:.3f}".format(diarizationErrorRate(gt, predictions, uem=Segment(0, len(test_inputs)))))
+    for segment in segment_sizes:
+        print("segment size: ", segment)
+        for i in range(len(test_inputs)):
+            embedding = embeddings[i]
+            gt[Segment(i, i + segment)] = test_labels[i]
+            predictions[Segment(i, i + segment)] = clustering.labels_[
+                i
+            ]  # labels[i]  # clustering.labels_[i]
 
+        print("Finished annotations")
 
+        diarizationErrorRate = DiarizationErrorRate()
+        diarizationPurity = DiarizationPurity()
+        DER = diarizationErrorRate(
+            gt, predictions, uem=Segment(0, len(test_inputs)), detailed=True
+        )
+        DP = diarizationPurity.compute_components(
+            gt, predictions, uem=Segment(0, len(test_inputs)), detailed=True
+        )
+        # print("DER = {0:.3f}".format(DER))
+        print(DER)
+        print(DP)
--- preprocessing.py
+++ preprocessing.py
@@ -4,30 +4,36 @@ import pickle
 import librosa
 import numpy as np
 import pandas as pd
+import time
 
-#TODO change these filepaths
-TRAIN_PATH_DIR_IN = "train_audio/"
-TRAIN_PATH_DIR_OUT = "train_features/"
+# TODO change these filepaths
+TRAIN_PATH_DIR_IN = "train_audio2/JL(wav+txt)/"
+TRAIN_PATH_DIR_OUT = "test_features/"
 TRAIN_PATH_DIR_OUT_LABELS = TRAIN_PATH_DIR_OUT + "labels.pkl"
-TRAIN_PATH_LABELS = "wav2spk.txt"
+TRAIN_PATH_LABELS = "wav2spk_test.txt"
 
 
 def features_extractor(file_name):
     audio, sample_rate = librosa.load(file_name, res_type="kaiser_fast")
 
-    #MFCCs
+    # MFCCs
     mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
     mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
 
     return mfccs_scaled_features
 
-df = pd.read_csv(TRAIN_PATH_LABELS, sep=' ', header=None, names=["Audio File", "Speaker ID"])
+
+df = pd.read_csv(
+    TRAIN_PATH_LABELS, sep=" ", header=None, names=["Audio File", "Speaker ID"]
+)
 df["Audio Features"] = df["Audio File"].apply(
     lambda filename: filename.split("/")[-1].replace(".wav", ".npy")
 )
-file_to_speaker_id = dict(zip(df['Audio Features'], df['Speaker ID']))
+file_to_speaker_id = dict(zip(df["Audio Features"], df["Speaker ID"]))
 with open(TRAIN_PATH_DIR_OUT_LABELS, "wb") as f:
     pickle.dump(file_to_speaker_id, f)
+
+start_time = time.time()
 
 count = 0
 for root, dirs, files in os.walk(TRAIN_PATH_DIR_IN):
@@ -39,4 +45,6 @@ for root, dirs, files in os.walk(TRAIN_PATH_DIR_IN):
             np.save(new_filepath, features)
             count += 1
             print(new_filepath, count)
+end_time = time.time()
 print("Total samples with features extracted: ", count)
+print("Total Time: {} seconds", end_time - start_time)
