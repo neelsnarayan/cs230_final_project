f7c9609e13abf32f3f2cbdee6dd236f73b032143
neelsnarayan <114615461+neelsnarayan@users.noreply.github.com>
2022-12-09 23:57:42 -0800

    Update clustering.py

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,18 +1,32 @@
-import tensorflow.keras as keras
+"""
+clustering.py is used for testing our model on the unseen JL corpus dataset. We
+train our model on the Flickr Dataset and then use spectral clustering built on
+our LSTM model to group the speakers in the JL corpus into 4 groups, since there
+are 4 speakers present.
+
+NOTE:
+This version of clustering.py works for our LSTM embeddings. However, we've slightly
+modified our clustering algorithm for compliance with our SimCLR output.
+"""
+
+import keras as keras
+import numpy as np
+import pandas as pd
 from pyannote.core import Annotation, Segment
 from pyannote.metrics.diarization import DiarizationErrorRate, DiarizationPurity
 from sklearn.cluster import SpectralClustering
-import numpy as np
-import pandas as pd
 import time
 
 MODEL_NAME = "softmax_sparse_categorical_crossentropy"
 TEST_PATH_LABELS = "wav2spk_test.txt"
 TEST_PATH_DIR_OUT = "test_features/"
 
-#NOTE: For simclr version, load the simclr model instead (example in notebooks)
 
 def load_test_data():
+    """
+    Here, we load our test data and ensure that our labels match our inputs. We return
+    two arrays containing our audio files and the speaker for each audio file
+    """
     df = pd.read_csv(
         TEST_PATH_LABELS, sep=" ", header=None, names=["Audio File", "Speaker ID"]
     )
@@ -20,19 +34,22 @@ def load_test_data():
         lambda filename: filename.split("/")[-1].replace(".wav", ".npy")
     )
 
+    # load input and label arrays
     inputs = []
     labels = []
     file_to_speaker_id = dict(zip(df["Audio Features"], df["Speaker ID"]))
     for j, filename in enumerate(file_to_speaker_id):
         try:
-            input = np.load(TEST_PATH_DIR_OUT + filename)
+            input_file = np.load(TEST_PATH_DIR_OUT + filename)
             try:
-                inputs.append(input)
+                inputs.append(input_file)
                 labels.append(file_to_speaker_id[filename] - 1)
             except:
-                print("Couldn't open the label for some reason")
+                print("Couldn't open the label")
+                raise
         except:
             print("This input isn't part of our feature set yet")
+            raise
 
     inputs = np.array(inputs)
     labels = np.array(labels)
@@ -42,6 +59,15 @@ def load_test_data():
 
 if __name__ == "__main__":
 
+    """
+    START MODEL
+    """
+    """
+    We create a sequential model with two LSTM layers (each with 64 units),
+    two dense layers (one with 350 units and one with 500 units) with dropout
+    present in the 350 unit dense layer (keep probability of 70%), and a final
+    activation layer. ReLU and softmax activations are used.
+    """
     # model = keras.Sequential()
 
     # model.add(keras.layers.LSTM(64, return_sequences=True))
@@ -52,18 +78,23 @@ if __name__ == "__main__":
 
     # model.add(keras.layers.Dense(500))
     # model.add(keras.layers.Activation(keras.activations.softmax))
+    """
+    END MODEL
+    """
 
+    """
+    Since we've already trained the model, we go ahead and load it
+    """
     # load the model
     model = keras.models.load_model(MODEL_NAME)
-    # print(model.layers[-1].output)
-    # print(len(model.layers))
+    # status checks
     print("Finished loading model")
 
-    # pop off the last dense + softmax layer (maybe change it to just pop off the softmax if possible) and recompile
+    # pop off the last dense + softmax layer - design choice
     # model.pop()
-    # print(model.layers[-1].output)
-    # print(len(model.layers))
     # print("Popped off last dense + softmax")
+
+    # generate optimizer
     optimiser = keras.optimizers.Adam(learning_rate=0.0001)
     model.compile(
         optimizer=optimiser,
@@ -79,10 +110,10 @@ if __name__ == "__main__":
     embeddings = model.predict(test_inputs, batch_size=32)
     print(test_inputs.shape)
     print(embeddings.shape)
-    # labels = np.argmax(embeddings, axis=1)
     print("Finished predictions")
 
     start = time.time()
+    # we use 4 clusters since there are 4 speakers in the JL Corpus Dataset (our test set)
     clustering = SpectralClustering(
         n_clusters=4, assign_labels="discretize", random_state=0, verbose=True
     ).fit(embeddings)
@@ -100,12 +131,11 @@ if __name__ == "__main__":
         for i in range(len(test_inputs)):
             embedding = embeddings[i]
             gt[Segment(i, i + segment)] = test_labels[i]
-            predictions[Segment(i, i + segment)] = clustering.labels_[
-                i
-            ]  # labels[i]  # clustering.labels_[i]
+            predictions[Segment(i, i + segment)] = clustering.labels_[i]
 
         print("Finished annotations")
 
+        # calculate metrics
         diarizationErrorRate = DiarizationErrorRate()
         diarizationPurity = DiarizationPurity()
         DER = diarizationErrorRate(
@@ -114,6 +144,5 @@ if __name__ == "__main__":
         DP = diarizationPurity.compute_components(
             gt, predictions, uem=Segment(0, len(test_inputs)), detailed=True
         )
-        # print("DER = {0:.3f}".format(DER))
         print(DER)
         print(DP)
